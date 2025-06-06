fd468413ed1fbe7445fc145e9a9ef7f35754131d
KaMoMaKi <lukaknott49@gmail.com>
2025-06-06 10:26:00 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Würfeln()
+    radio.sendNumber(zufall)
+})
+function Würfeln () {
+    zufall = randint(1, 3)
+}
+let zufall = 0
+radio.setFrequencyBand(20)
+radio.setGroup(1)
+radio.setTransmitSerialNumber(true)
+basic.forever(function () {
+	
+})
